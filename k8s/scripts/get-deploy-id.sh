705b878ba45c973fb5ff8c72de37d18ca6ee9d5d
Douglas Duteil <douglasduteil@gmail.com>
2019-04-03 09:40:21 +0200

    ci(gitlab): jobs factorisation XLXXVII

--- k8s/scripts/get-deploy-id.sh
+++ k8s/scripts/get-deploy-id.sh
@@ -4,17 +4,17 @@ set -eu -o pipefail
 CACHE_RESPONSE=${CACHE_RESPONSE:="/tmp/deploy_payload.json"}
 DEPLOY_ID_FILE=${DEPLOY_ID_FILE:="DEPLOY_ID"}
 
-curl -0 \
--X POST https://$GITHUB_TOKEN@api.github.com/repos/$CI_PROJECT_PATH/deployments \
+curl -0 -sS \
+-X POST "https://${GITHUB_TOKEN}@api.github.com/repos/${CI_PROJECT_PATH}/deployments" \
 -H "Content-Type:application/json" \
 -H "Accept: application/vnd.github.flash-preview+json, application/vnd.github.ant-man-preview+json" \
--o "$CACHE_RESPONSE" \
+-o "${CACHE_RESPONSE}" \
 -d @- << EOF
 {
   "auto_merge": false,
-  "description": "Deplying $CI_PROJECT_PATH@$CI_COMMIT_SHORT_SHA",
-  "environment": "$ENVIRONMENT",
-  "ref": "$CI_COMMIT_REF_NAME",
+  "description": "Deplying ${CI_PROJECT_PATH}@${CI_COMMIT_SHORT_SHA}",
+  "environment": "${ENVIRONMENT}",
+  "ref": "${CI_COMMIT_REF_NAME}",
   "required_contexts": [],
   "transient_environment": true
 }
