3f4c4bacf6e7d5e5d5cc6af3d6f4cc13127f4498
Douglas Duteil <douglasduteil@gmail.com>
2019-04-03 09:21:13 +0200

    ci(gitlab): jobs factorisation XLXXV

--- k8s/scripts/get-deploy-id.sh
+++ k8s/scripts/get-deploy-id.sh
@@ -5,21 +5,22 @@ CACHE_RESPONSE=${CACHE_RESPONSE:="/tmp/deploy_payload.json"}
 DEPLOY_ID_FILE=${DEPLOY_ID_FILE:="DEPLOY_ID"}
 
 curl -0 --trace-ascii - \
--X POST https://${GITHUB_TOKEN}@api.github.com/repos/${CI_PROJECT_PATH}/deployments \
+-X POST https://$GITHUB_TOKEN@api.github.com/repos/$CI_PROJECT_PATH/deployments \
 -H "Content-Type:application/json" \
 -H "Accept: application/vnd.github.flash-preview+json, application/vnd.github.ant-man-preview+json" \
--o "${CACHE_RESPONSE}" \
+-o "$CACHE_RESPONSE" \
 -d @- << EOF
 {
   "auto_merge": false,
-  "description": "Deplying ${CI_PROJECT_PATH}@${CI_COMMIT_SHORT_SHA}",
-  "environment": "${ENVIRONMENT}",
-  "ref": "${CI_COMMIT_REF_NAME}",
+  "description": "Deplying $CI_PROJECT_PATH@$CI_COMMIT_SHORT_SHA",
+  "environment": "$ENVIRONMENT",
+  "ref": "$CI_COMMIT_REF_NAME",
   "required_contexts": [],
   "transient_environment": true
 }
 EOF
 
-cat "${CACHE_RESPONSE}" | \
+cat "$CACHE_RESPONSE" | \
   python -c "import json,sys;obj=json.load(sys.stdin);print(obj.get('id'))" \
-  > "${DEPLOY_ID_FILE}"
+  > "$DEPLOY_ID_FILE"
+
