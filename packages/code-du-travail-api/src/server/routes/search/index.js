7c9f66b1e74c616a3840a00bfbe91d2b27e25fe6
Armand Giraud <armand.giraud.ag@gmail.com>
2019-05-15 15:23:59 +0200

    modify the index.js from search

--- packages/code-du-travail-api/src/server/routes/search/index.js
+++ packages/code-du-travail-api/src/server/routes/search/index.js
@@ -1,14 +1,10 @@
+const fetch = require("node-fetch").default;
+
 const Router = require("koa-router");
 const API_BASE_URL = require("../v1.prefix");
-
-const elasticsearchClient = require("../../conf/elasticsearch.js");
-const getSearchBody = require("./search.elastic");
-const getFacetsBody = require("./facets.elastic");
-
-const index =
-  process.env.ELASTICSEARCH_DOCUMENT_INDEX || "code_du_travail_numerique";
-
+ 
 const router = new Router({ prefix: API_BASE_URL });
+
 
 /**
  * Return documents matching the given query.
@@ -22,35 +18,13 @@ const router = new Router({ prefix: API_BASE_URL });
  */
 router.get("/search", async ctx => {
   const query = ctx.request.query.q;
-  const size = Math.min(ctx.request.query.size || 10, 100);
-
-  const excludeSources = (ctx.request.query.excludeSources || "").split(",");
-
-  const body = getSearchBody({ query, size, excludeSources });
-  const facetBody = getFacetsBody({ query });
-
-  // query data
-  const results = await elasticsearchClient.search({ index, body });
+  const response = await fetch(
+    `http://0.0.0.0:5005/api/search?q=${query}`
+  );
+  const results = await response.json();
   ctx.body = {
-    hits: {
-      ...results.hits,
-      hits: results.hits.hits.filter(item => item._source.source !== "snippet")
-    },
-    facets: []
+    ...results
   };
-  if (results.aggregations.bySource.buckets.length > 0) {
-    const [snippetResults] = results.aggregations.bySource.buckets;
-    ctx.body.snippet = snippetResults.bySource.hits.hits[0];
-  }
-
-  // facet data
-  const facetResults = await elasticsearchClient.search({
-    index,
-    body: facetBody
-  });
-  if (facetResults.aggregations.document_count.buckets.length > 0) {
-    ctx.body.facets = facetResults.aggregations.document_count.buckets;
-  }
 });
 
 module.exports = router;
